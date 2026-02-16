a1a2c1649a23555be1dae7e5290a6fddcab988e4
siwaphon <68011077@kmitl.ac.th>
2026-02-16 10:24:36 +0700

    p3

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -11,7 +11,7 @@ export class Playstation extends Product {
   }
 
   getProfile(): string{
-    return `${this.name} (Gen ${this.generation})`;
+    return `${this.name} (gen ${this.generation})`;
   }
 
   getDiscountPrice(): number {
