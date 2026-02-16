d3d10d410203d660b1d507cf907edff7c2489f9e
siwaphon <68011077@kmitl.ac.th>
2026-02-16 10:16:03 +0700

    p3

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,22 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+  private generation: number;
+
+  constructor(name: string, generation: number, price: number){
+    super(name, price);
+
+    this.generation = generation;
+  }
+
+  getProfile(): string{
+    return `${this.name} (Gen ${this.generation})`;
+  }
+
+  getDiscountPrice(): number {
+    const discountAmount = (this.price * Product.DISCOUNT_PERCENT)/100;
+    return this.price - discountAmount;
+  }
+
+
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,21 @@
-export class Product {}
+export class Product {
+    readonly name: string;
+    protected price: number;
+    static readonly DISCOUNT_PERCENT: number = 10;
+    
+    constructor(name: string, price: number = 0) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+getPrice(): number {
+    return this.price;
+}
+
+setPrice(price: number): void {
+    this.price = price;
+}
+}
+
 
