0e725be67fa7b64fff54e0115a88b0ab60147682
siwaphon <68011077@kmitl.ac.th>
2026-02-16 09:46:26 +0700

    p1p2

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,9 @@
-export class Student {}
+export class Student {
+    firstname: string = "";
+    lastname: string = "";
+
+    printName(): void {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+}
+
--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    firstname: string = "";
+    lastname: string = "";
+    private _age: number = 0;
+    static COUNTRY: string = "Thailand";
+
+    setAge(age: number): void {
+        this._age = age;
+    }
+
+    getAge(): number  {
+       return this._age;
+    }
+
+    getFullName(): string{
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
